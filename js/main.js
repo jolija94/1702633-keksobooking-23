caeb5d87a761e28d8635a9bf6713f9d2aa8fcc1f
jolija94 <angelok19.as@gmail.com>
2021-05-30 19:21:02 +0300

    добавляет функции, которые показывают рандомные числа в заданном интервале

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min > max){
+    const chosenNumber = min;
+    min = max;
+    max = chosenNumber;
+  }
+
+  const number = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  if (number < 0) {
+    return 'too bad';
+  }
+  return number;
+}
+
+getRandomIntInclusive();
+
+function getRandom(min, max, fix) {
+  if (min > max) {
+    const chosenNumber = min;
+    min = max;
+    max = chosenNumber;
+  }
+  const number = Math.random() * (max - min) + min;
+  if (number < 0) {
+    return 'very bad';
+  }
+  return [number.toFixed(fix), fix];
+}
+
+getRandom();
