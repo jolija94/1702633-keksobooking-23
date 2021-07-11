b73c939fb540bae8b9f9ddde907f6dbe3aa9adc2
jolija94 <angelok19.as@gmail.com>
2021-07-11 16:36:31 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import {TYPES_ARRAY, HOURS_ARRAY, FEATURES_ARRAY, PHOTOS_ARRAY, TITLES_ARRAY, DESCRIPTION_ARRAY, ADT,  getRandomListArray, createAdt, createSomeAdt} from './data.js';
+import {TYPES_ARRAY, HOURS_ARRAY, FEATURES_ARRAY, PHOTOS_ARRAY, TITLES_ARRAY, DESCRIPTION_ARRAY, ADT, getRandomListArray, createAdt, createSomeAdt} from './data.js';
 import {getRandomIntInclusive, getRandom, getRandomElementArray} from './util.js';
-import {userTemplateFragment, housingType, setAdt, createNewAdt} from './popup.js';
+import {userTemplateFragment, housingType, setAdt, setAdtElementContent, createNewAdt} from './popup.js';
