3cb9a84f6da72e6f40b3d6c0ea26d0f7cca89005
jolija94 <angelok19.as@gmail.com>
2021-07-15 21:43:21 +0300

    Отрисуй меня полностью (часть 1)

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,9 @@
+import {createSomeAdt} from './data.js';
+
 const map = document.querySelector('.map');
 const mapCanvas = map.querySelector('#map-canvas');
-const userPopupTemplate = document.querySelector('#card').content.querySelector('.popup');
-
+const userTemplateFragment = document.querySelector('#card').content.querySelector('.popup');
+const popupFragment = document.createDocumentFragment();
 const housingTypes = {
   flat:'Квартира',
   bungalow:'Бунгало',
@@ -13,7 +15,8 @@ const housingTypes = {
 const getTypeHousesRussian = (housingType) => housingTypes[housingType];
 
 const setAdt = (adtElement, visible) => {
-  if (!visible) {
+  if (visible) {
+
     adtElement.classList.remove('hidden');
     return;
   }
@@ -28,45 +31,45 @@ const  setAdtElementContent = (adtElement, content, isHtml) => {
   adtElement.textContent = content;
 };
 
-const createPromo = (createSomeAdt) => createSomeAdt.map((adtElement) => {
-  const popup =  userPopupTemplate.cloneNode(true);
+createSomeAdt.map((adtElement) => {
+  const popup = userTemplateFragment.cloneNode(true);
 
   const title = popup.querySelector('.popup__title');
-  setAdt(title, adtElement.offer.title.textContent);
+  setAdt(title, adtElement.offer.title);
   setAdtElementContent(title, adtElement.offer.title, false);
 
   const address = popup.querySelector('.popup__text--address');
-  setAdt(address, adtElement.offer.address.textContent);
+  setAdt(address, adtElement.offer.address);
   setAdtElementContent(address, adtElement.offer.address, false);
 
-  const type = popup.querySelector('.popup_type');
-  setAdt(type, adtElement.offer.types.textContent);
+  const type = popup.querySelector('.popup__type');
+  setAdt(type, adtElement.offer.types);
   setAdtElementContent(type, getTypeHousesRussian(adtElement.offer.type), false);
 
   const price = popup.querySelector('.popup__text--price');
-  setAdt(price, adtElement.offer.price.textContent);
-  setAdtElementContent(price, `${adtElement.offer.price} <span> ₽/ночь </span>`, false);
+  setAdt(price, adtElement.offer.price);
+  setAdtElementContent(price, `${adtElement.offer.price} <span>₽/ночь</span>`, true);
 
-  const capacity = popup.querySelector('.popup__text--capacity').textContent;
+  const capacity = popup.querySelector('.popup__text--capacity');
   setAdt(capacity, adtElement.offer.rooms && adtElement.offer.guests);
   setAdtElementContent(capacity, `${adtElement.offer.rooms} комнаты для ${adtElement.offer.guests} гостей`, false);
 
   const time = popup.querySelector('.popup__text--time');
   setAdt(time, adtElement.offer.checkin && adtElement.offer.checkout);
-  setAdtElementContent(time, ` Заезд после ${adtElement.offer.checkin.textContent}, выезд до ${adtElement.offer.checkout.textContent} `, false);
-
-  const description = popup.querySelector('.popup__description');
-  setAdt(description, adtElement.offer.features.textContent);
-  setAdtElementContent(description, adtElement.popup.description, false);
+  setAdtElementContent(time,  `Заезд после ${adtElement.offer.checkin}, выезд до ${adtElement.offer.checkout}`, false);
 
   const avatar = popup.querySelector('.popup__avatar');
-  setAdt(avatar, adtElement.autor.avatar);
-  avatar.src = adtElement.autor.avatar;
+  setAdt(avatar, adtElement.author.avatar);
+  avatar.src = adtElement.author.avatar;
 
-  const features = popup.querySelectorAll('.popup__features');
-  if (adtElement.offer.features) {
-    const featuresPopup = adtElement.offer.features;
-    setAdt(features, featuresPopup.length);
+  const description = popup.querySelector('.popup__description');
+  setAdt(description, adtElement.offer.features);
+  setAdtElementContent(description, adtElement.offer.description, false);
+
+  const features = popup.querySelector('.popup__features');
+  const featuresPopup = adtElement.offer.features;
+  setAdt(features, featuresPopup && featuresPopup.length);
+  if (featuresPopup) {
     features.innerHTML = '';
     featuresPopup.forEach((featurePopup) =>  {
       const featuresItem  = document.createElement('li');
@@ -76,21 +79,24 @@ const createPromo = (createSomeAdt) => createSomeAdt.map((adtElement) => {
     });
   }
 
-  const photoList = popup.querySelectorAll('.popup__photos');
-  const templatePhotosGallery = popup.querySelector('.popup__photos');
-  if (adtElement.offer.photos) {
-    const photosPopup = adtElement.offer.photos;
-    setAdt(photoList, photosPopup.length);
-    photoList.innerHTML = '';
-    photosPopup.forEach ((photoPopup) =>  {
-      const photoItem = templatePhotosGallery.cloneNode(true);
-      photoItem.src = photoPopup;
-      photoList.appendChild(photoItem);
+  const photos = popup.querySelector('.popup__photos');
+  const photosPopup = adtElement.offer.features;
+  setAdt(photos, photosPopup && photosPopup.length);
+
+  if (photosPopup) {
+    photos.innerHTML = '';
+    adtElement.offer.photos.forEach((photo) =>  {
+      const photoPopup  = document.createElement('img');
+      photoPopup.classList.add('popup__photo');
+      photoPopup.width = 45;
+      photoPopup.height = 40;
+      photoPopup.src = photo;
+      photos.appendChild(photoPopup);
     });
   }
-  return popup;
+
+  popupFragment.appendChild(popup);
 });
 
-mapCanvas.appendChild();
+mapCanvas.appendChild(popupFragment);
 
-export {createPromo, mapCanvas, map};
