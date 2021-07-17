f728758c862393c58ad63de588398ff196efdb7f
jolija94 <angelok19.as@gmail.com>
2021-07-17 22:18:59 +0300

    Правда или действие (часть 1 и часть 2)

--- js/form.js
+++ js/form.js
@@ -22,17 +22,98 @@ const toggleDisabledPage = (inactive) => {
 
 export {toggleDisabledPage};
 
+import {MAX_PRICE, MIN_PRICE, PRICE_TYPE, NOT_GUESTS, MAX_ROOM} from './data.js';
+import {sameValue} from './util.js';
 
-// const titleAdt = document.querySelector('#title');
-// const LENGTH_MIN_TITLE = 30;
-// const LENGTH_MAX_TITLE = 100;
+const priceAdt = document.querySelector('#price');
+priceAdt.addEventListener('input', () => {
+  const costPrice = priceAdt.value;
+  if (costPrice < MIN_PRICE) {
+    priceAdt.setCustomValidity(`Минимальная цена ${MIN_PRICE} ₽/ночь`);
+  } else if (costPrice > MAX_PRICE) {
+    priceAdt.setCustomValidity(`Максимальная цена превышается на ${costPrice - MAX_PRICE}₽. Максимально возможная цена ${MAX_PRICE} ₽/ночь`);
+  } else {
+    priceAdt.setCustomValidity('');
+  }
+  priceAdt.reportValidity('');
+});
 
-// const priceAdt = document.querySelector('#price');
-// const MAX_PRICE = 1000000;
+const roomNumber = document.querySelector('#room_number');
+const roomCapacityGuests = document.querySelector('#capacity');
 
-// const roomNumber = document.querySelector('#room_number');
-// const capacityRoom = document.querySelector('#capacity');
-// const roomCapacityOptions = document.querySelector('#option');
-// const roomCapacityGuests = document.querySelector('#value')
-// const ROOM_NO_GUESTS = document.querySelector('value')
+const checkCapacity = (input) => {
+  if (
+    Number(roomNumber.value) === MAX_ROOM && Number(roomCapacityGuests.value) !== NOT_GUESTS
+  ) {
+    input.setCustomValidity(`${MAX_ROOM} комнат нельзя выбрать для гостей!`);
+  } else if (
+    Number(roomNumber.value) !== MAX_ROOM && Number(roomCapacityGuests.value) === 0
+  ) {
+    input.setCustomValidity(`Не для гостей доступно только ${MAX_ROOM} комнат!`);
+  } else if (
+    (Number(roomNumber.value) !== MAX_ROOM) && Number(roomCapacityGuests.value) !== 0 && Number(roomNumber.value) < Number(roomCapacityGuests.value)
+  ) { input.setCustomValidity('Количество гостей не должно превышать количество комнат!');
+  } else {
+    input.setCustomValidity('');
+  }
+  input.reportValidity('');
+};
 
+const arrivalTimeIn = document.querySelector('#timein');
+const departureTimeOut = document.querySelector('#timeout');
+
+arrivalTimeIn.addEventListener('change', () =>
+  sameValue(arrivalTimeIn, departureTimeOut),
+);
+departureTimeOut.addEventListener('change', () =>
+  sameValue(departureTimeOut, arrivalTimeIn),
+);
+
+const houseType = document.querySelector('#type');
+roomNumber.addEventListener('change', () => {
+  if (roomNumber.value < roomCapacityGuests.value && roomNumber.value !== MAX_ROOM){
+    roomNumber.value = roomCapacityGuests.value;
+  } if (Number(roomNumber.value) === MAX_ROOM){
+    roomCapacityGuests.value = NOT_GUESTS;
+  }
+  checkCapacity(roomNumber);
+});
+roomCapacityGuests.addEventListener('change', () => {
+  if (Number(roomCapacityGuests.value) === NOT_GUESTS) {
+    roomNumber.value = MAX_ROOM;
+  }
+  checkCapacity(roomCapacityGuests);
+});
+
+houseType.addEventListener('change', () => {
+  const value = PRICE_TYPE[houseType.value.toUpperCase()];
+  switch(houseType.value) {
+    case 'flat':
+      priceAdt.setAttribute('min', value);
+      priceAdt.setAttribute('placeholder', value);
+      break;
+    case 'bungalow':
+      priceAdt.setAttribute('min', value);
+      priceAdt.setAttribute('placeHolder', value);
+      break;
+    case 'house':
+      priceAdt.setAttribute('min', value);
+      priceAdt.setAttribute('placeholder', value);
+      break;
+    case 'palace':
+      priceAdt.setAttribute('min', value);
+      priceAdt.setAttribute('placeholder', value);
+      break;
+    case 'hotel':
+      priceAdt.setAttribute('min', value);
+      priceAdt.setAttribute('placeholder', value);
+      break;
+  }
+});
+
+const valueFlat = PRICE_TYPE[houseType.value.toUpperCase()];
+
+if (houseType.value === 'flat') {
+  priceAdt.setAttribute('min', valueFlat);
+  priceAdt.setAttribute('placeholder', valueFlat );
+}
