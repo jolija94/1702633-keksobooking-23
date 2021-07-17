2df02b9c328d1ee1b339009b687671e4347957ba
jolija94 <angelok19.as@gmail.com>
2021-07-17 14:50:40 +0300

    добавляет функцию неактивного/активного состояния страницы

--- js/form.js
+++ js/form.js
@@ -1,42 +1,38 @@
-const formAdt = document.querySelector('.ad-form');
-const headerFormAdt = document.querySelector('.ad-form-header');
-const elementsFormAdt = document.querySelector('.ad-form__element');
-// const buttonSubmitFormAdt = document.querySelector('.ad-form__submit');
-// const buttonResetAdt = document.querySelector('.ad-form__reset');
-
+const formAdt = document.querySelector('.ad-form').querySelectorAll('fieldset');
 const formMap = document.querySelector('.map__filters');
-const filtersMap = document.querySelector('.map__filter');
 const featuresMap = document.querySelector('.map__features');
+const getFiltersSelects = formMap.querySelectorAll('select');
 
 const toggleDisabledPage = (inactive) => {
+  formAdt.forEach((item) => {
+    item.disabled = inactive;
+  });
+  getFiltersSelects.forEach((item) => {
+    item.disabled = inactive;
+  });
+  featuresMap.disabled = inactive;
   if (inactive) {
-    formAdt.classList.add('ad-form--disabled');
-    formMap.classList.add('.map__filters--disabled');
+    document.querySelector('.ad-form').classList.add('ad-form--disabled');
+    formMap.classList.add('map__filters--disabled');
   } else {
-    formAdt.classList.remove('ad-form--disabled');
+    document.querySelector('.ad-form').classList.remove('ad-form--disabled');
     formMap.classList.remove('map__filters--disabled');
   }
-  headerFormAdt.disabled = inactive;
-  elementsFormAdt.forEach((elementFormAdt) => {elementFormAdt.disabled = inactive;});
-  // buttonSubmitFormAdt.disabled = inactive;
-  // buttonResetAdt.disabled = inactive;
-  filtersMap.forEach((filterMap) => {filterMap.disabled = inactive;});
-  featuresMap.disabled = inactive;
 };
 
-toggleDisabledPage(true);
+export {toggleDisabledPage};
 
 
-const titleAdt = document.querySelector('#title');
-const LENGTH_MIN_TITLE = 30;
-const LENGTH_MAX_TITLE = 100;
+// const titleAdt = document.querySelector('#title');
+// const LENGTH_MIN_TITLE = 30;
+// const LENGTH_MAX_TITLE = 100;
 
-const priceAdt = document.querySelector('#price');
-const MAX_PRICE = 1000000;
+// const priceAdt = document.querySelector('#price');
+// const MAX_PRICE = 1000000;
 
-const roomNumber = document.querySelector('#room_number');
-const capacityRoom = document.querySelector('#capacity');
-const roomCapacityOptions = document.querySelector('#option');
-// conat roomCapacityGuests = document.querySelector('#value')
-const ROOM_NO_GUESTS = document.querySelector('value')
+// const roomNumber = document.querySelector('#room_number');
+// const capacityRoom = document.querySelector('#capacity');
+// const roomCapacityOptions = document.querySelector('#option');
+// const roomCapacityGuests = document.querySelector('#value')
+// const ROOM_NO_GUESTS = document.querySelector('value')
 
--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ import './data.js';
 import './util.js';
 import './popup.js';
 import './form.js';
+import {toggleDisabledPage} from './form.js';
+
+toggleDisabledPage(true);
